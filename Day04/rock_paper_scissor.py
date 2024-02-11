7c1877dfa937ec5a7834768ec16f8b1a300ac0f1
Natanim Issa <natanimissa@gmail.com>
2024-02-10 22:28:56 -0500

    fixed the invalid value bug

--- Day04/rock_paper_scissor.py
+++ Day04/rock_paper_scissor.py
@@ -26,33 +26,25 @@ scissors = '''
 ---.__(___)
 '''
 
+game_images = [rock, paper, scissors]
 print("What do you choose? Type 0 for rock, 1 for paper, 2 for scissors: \n")
 choice = int(input())
 comp_choice = random.randint(0,2)
-if choice == 0:
-    print(rock)
-elif choice == 1:
-    print(paper)
-elif choice == 2:
-    print(scissors)
-
-print("Computer choice: \n")
-if comp_choice == 0:
-    print(rock)
-elif comp_choice == 1:
-    print(paper)
-elif comp_choice == 2:
-    print(scissors)
-
-if choice == 0 and comp_choice == 2:
-    print("YOU WON!!!")
-elif comp_choice == 0 and choice == 2:
-    print("YOU LOSE!!!")
-if choice == 1 and comp_choice == 0:
-    print("YOU WON!!!")
-if comp_choice == 1 and choice == 0:
-    print("YOU LOSE!!!")
-if choice == 2 and comp_choice == 1:
-    print("YOU WON!!!")
-if comp_choice == 2 and choice == 1:
-    print("YOU LOSE!!!")
+if choice > 2 or choice < 0:
+    print("WRONG INPUT!!!")
+    print("What do you choose? Type 0 for rock, 1 for paper, 2 for scissors: \n")
+    choice = int(input())
+else:
+    print(game_images[choice])
+    print("Computer choice: \n")
+    print(game_images[comp_choice])
+    if choice == 0 and comp_choice == 2:
+        print("YOU WON!!!")
+    elif comp_choice == 0 and choice == 2:
+        print("YOU LOSE!!!")
+    elif comp_choice > choice == 2:
+        print("YOU LOSE!!!")
+    elif choice > comp_choice:
+        print("YOU WON!!!")
+    elif comp_choice == choice:
+        print("DRAW!!!")
