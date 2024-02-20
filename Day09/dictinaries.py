e7f0c273ac32223adf671d9ece77f5163067a5c0
Natanim Issa <natanimissa@gmail.com>
2024-02-20 03:21:03 -0500

    Day 9 completed

---
+++ Day09/dictinaries.py
@@ -0,0 +1,15 @@
+# Dictionaries in python are used to store key:value pair data
+# Is ordered, changable and don't allow duplicate
+
+# Empty dictionary
+my_dictionary = {}
+
+programming_concepts = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can call and run again and again."
+}
+
+# Adding a new value to a dictionary
+# Adds the value related related to the key into dictionary if it doesn't exist
+# Or updates the value related to the key if it exists
+programming_concepts["Loop"] = "The action of doing something over and over again."
---
+++ Day09/silent_auction.py
@@ -0,0 +1,30 @@
+from auction_logo import logo
+import os
+
+print(logo)
+print("Welcome to the Secret Auction Program: ")
+bid_continue = True
+bid_auction = {}
+
+
+def silent_auction(bid_auctions):
+    winner_name = ""
+    winner_amount = 0
+    for key in bid_auctions:
+        if bid_auctions[key] > winner_amount:
+            winner_name = key
+            winner_amount = bid_auctions[key]
+    print(f"The winner is {winner_name} with a bid of ${winner_amount}.")
+
+
+while bid_continue:
+    name = input("What is your name: ")
+    bid_amount = int(input("What is your bid: $"))
+    bid_auction[name] = bid_amount
+    choice = input("Are there any other bidders, Type 'yes' or 'no': ")
+    if choice == "no":
+        bid_continue = False
+        silent_auction(bid_auction)
+
+
+
